f6d150842f334b28b60df56a005cccad6b4e67ee
Kush Aheer <kushaheer709@gmail.com>
2024-09-21 01:28:15 +0530

    typing mistake

--- Backend/server.js
+++ Backend/server.js
@@ -27,14 +27,14 @@ app.use(cors( ));
 app.use('/api/auth', authRoutes);
 app.use('/api/messages',messageRoutes)
 app.use('/api/user',userRoutes);
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, '/Frontend/dist')));
 
 app.get('/', (req, res) => {
     res.send("Hello from express");
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, '/Frontend/dist/index.html'));
 });
 
 server.listen(PORT, () => {
